1a0efd5371003c3b03d70c32278cd5eb41b0d055
prafuljain123 <prafuljain303@gmail.com>
2024-03-20 17:11:16 +0530

    commit

--- push.sh
+++ push.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
 while sleep 5
 do
-git add .
-git commit -m "commit"
-git push
-echo "saving"
+git diff --quiet; nochanges=$?
+if [ $nochanges -eq 0 ]; then
+    continue
+else
+    git add .
+	git commit -m "commit"
+	git push
+	echo "saving"
+fi
 done
